44637afc4280306e904401f1e783e2690d4c6b1b
1p22geo <1p22geodecki@gmail.com>
2023-12-03 16:19:11 +0100

    gitignore'd pycache

--- model.py
+++ model.py
@@ -15,7 +15,10 @@ class Model():
 
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(256, activation='sigmoid'),
+            tf.keras.layers.Dense(192, activation='sigmoid'),
             tf.keras.layers.Dense(128, activation='sigmoid'),
+            tf.keras.layers.Dropout(0.2),
             tf.keras.layers.Dense(96, activation='sigmoid'),
             tf.keras.layers.Dense(96, activation='sigmoid'),
             tf.keras.layers.Dropout(0.2),
